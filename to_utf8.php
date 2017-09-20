870c6e8225e95a2f4605ab3e53c6e91ea50e8e78
prolin99 <prolin99@gmail.com>
2017-09-20 14:56:54 +0800

    php 版本

--- to_utf8.php
+++ to_utf8.php
@@ -1,28 +1,28 @@
-#!/usr/bin/php5 -q
+#!/usr/bin/php  -q
 <?php
 
-function checkUTF8($str) {       
+function checkUTF8($str) {
   // $str=file_get_contents($suc_file);
- 
-      if  ( ord($str[0])== 0xFF  and ord($str[1])== 0xFe ) 
-          return  "UTF-16LE" ;
-      if  ( ord($str[0])== 0xFE  and ord($str[1])== 0xFF) 
-          return  "UTF-16BE" ;    
-       
-      if  ( ord($str[0])== 0xFE  and ord($str[1])== 0xBB   and ord($str[2])== 0xBF ) 
-          return  "UTF-8" ;        
- 
-      for($i = 0; $i < strlen($str); $i++){       
-          $value = ord($str[$i]);       
-          if($value > 127) {       
-              if($value >= 192 && $value <= 247) return "UTF-8";       
-              else return (0);//.die('Not a UTF-8 compatible string');       
-          }       
-      }  
-      return (0);    
-} 
 
-//讀入資料 
+      if  ( ord($str[0])== 0xFF  and ord($str[1])== 0xFe )
+          return  "UTF-16LE" ;
+      if  ( ord($str[0])== 0xFE  and ord($str[1])== 0xFF)
+          return  "UTF-16BE" ;
+
+      if  ( ord($str[0])== 0xFE  and ord($str[1])== 0xBB   and ord($str[2])== 0xBF )
+          return  "UTF-8" ;
+
+      for($i = 0; $i < strlen($str); $i++){
+          $value = ord($str[$i]);
+          if($value > 127) {
+              if($value >= 192 && $value <= 247) return "UTF-8";
+              else return (0);//.die('Not a UTF-8 compatible string');
+          }
+      }
+      return (0);
+}
+
+//讀入資料
 $lines = file_get_contents('php://stdin');
 $code= checkUTF8( $lines) ;
 //  echo '===========' . $code ;
@@ -32,11 +32,11 @@ $code= checkUTF8( $lines) ;
 //  echo '===========' . $code ;
 
  echo    $lines  ;
- 
+
 if    ($code<>  "UTF-8" )
-     $lines =  iconv($code, "UTF-8", $lines) ;   
- 
- 
- 
+     $lines =  iconv($code, "UTF-8", $lines) ;
+
+
+
  echo    $lines  ;
 ?>
